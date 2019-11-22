17935d1c5c5ccd8c5d44b35d67a8f421e7077c15
Matheus Santos <matheus.santos@ilegra.com>
2019-11-22 16:06:37 -0300

    Added style based on the state.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/TheContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
         <ul>
-          <li>The Way of Kings</li>
-          <li>The Name of the Wind</li>
-          <li>The Final Empire</li>
+          <li style={{ background: theme.ui }}>The Way of Kings</li>
+          <li style={{ background: theme.ui }}>The Name of the Wind</li>
+          <li style={{ background: theme.ui }}>The Final Empire</li>
         </ul>
       </div>
     );
