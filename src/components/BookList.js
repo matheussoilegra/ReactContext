3e1e7c2c0031ee9669abf3f51b1ceb0ff580a7a3
Matheus Santos <matheus.santos@ilegra.com>
2019-11-25 13:33:41 -0300

    Added useContext.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
 
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-
-    return (
-      <div
-        className="book-list"
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The Way of Kings</li>
-          <li style={{ background: theme.ui }}>The Name of the Wind</li>
-          <li style={{ background: theme.ui }}>The Final Empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The Way of Kings</li>
+        <li style={{ background: theme.ui }}>The Name of the Wind</li>
+        <li style={{ background: theme.ui }}>The Final Empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
